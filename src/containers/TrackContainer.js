cc0cfe7899b4891b1c12c4b2b6283f8bcf9ffe4a
Guy Chalk <guychalk532@gmail.com>
2022-10-10 12:06:45 +0100

    fixing my changes

--- src/containers/TrackContainer.js
+++ src/containers/TrackContainer.js
@@ -3,6 +3,21 @@ import { useState, useEffect } from "react";
 
 const TrackContainer = () => {
 
+    const[tracks, setTracks] = useState([]);
+    
+    const loadTrackData = async () =>{
+        const response = await fetch("http://localhost:8080/tracks")
+        const data = await response.json()
+        setTracks(data)
+
+        //setTracks(data => setTracks(data))
+
+        console.log(data);
+
+    }
+
+    useEffect(() =>{loadTrackData()},[])
+    
     return (
         <>
             <h1>Track Container</h1>
