9196d3fd2de7b563c565cbe405ee2b34e0361ae3
Heying778 <yingyinghe2022@gmail.com>
2022-10-12 10:04:52 +0100

    added GenreList filter

--- src/components/GenresList.js
+++ src/components/GenresList.js
@@ -23,7 +23,7 @@ const GenresList = ({tracks, addToPlaylist}) => {
             { 
                 tracks
                 .filter(track => track.genre === 'JAZZ')
-                .map(tracks => <li key={tracks.trackId}>{tracks.name}</li>) 
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
                 
             }< br />
 
@@ -31,21 +31,21 @@ const GenresList = ({tracks, addToPlaylist}) => {
             { 
                 tracks
                 .filter(track => track.genre === 'ROCK')
-                .map(tracks => <li key={tracks.trackId}>{tracks.name}</li>) 
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
             }<br />
 
             <h4>BLUES:</h4>
             { 
                 tracks
                 .filter(track => track.genre === 'BLUES')
-                .map(tracks => <li key={tracks.trackId}>{tracks.name}</li>) 
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
             }<br />
 
             <h4>COUNTRY:</h4>
             { 
                 tracks
                 .filter(track => track.genre === 'COUNTRY')
-                .map(tracks => <li key={tracks.trackId}>{tracks.name}</li>) 
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
             }<br />
 
             <h4>CLASSICAL:</h4>
@@ -54,10 +54,73 @@ const GenresList = ({tracks, addToPlaylist}) => {
                 .filter(track => track.genre === 'CLASSICAL')
                 .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
             }<br />
+            
+            <h4>ELECTRONIC:</h4>
+            { 
+                tracks
+                .filter(track => track.genre === 'ELECTRONIC')
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
+            }<br />
 
+
+            <h4>HIPHOP:</h4>
+            { 
+                tracks
+                .filter(track => track.genre === 'HIPHOP')
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
+            }<br />
+
+            <h4>METAL:</h4>
+            { 
+                tracks
+                .filter(track => track.genre === 'METAL')
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
+            }<br />
+
+            <h4>ALTERNATIVE:</h4>
+            { 
+                tracks
+                .filter(track => track.genre === 'ALTERNATIVE')
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
+            }<br />
+
+            <h4>SOUL:</h4>
+            { 
+                tracks
+                .filter(track => track.genre === 'SOUL')
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
+            }<br />
+
+            <h4>RNB:</h4>
+            { 
+                tracks
+                .filter(track => track.genre === 'RNB')
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
+            }<br />
+
+            <h4>FOLK:</h4>
+            { 
+                tracks
+                .filter(track => track.genre === 'FOLK')
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
+            }<br />
+
+            <h4>POP:</h4>
+            { 
+                tracks
+                .filter(track => track.genre === 'POP')
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
+            }<br />
+
+            <h4>FUNK:</h4>
+            { 
+                tracks
+                .filter(track => track.genre === 'FUNK')
+                .map(tracks => <li key={tracks.trackId}>{tracks.name} <button onClick={handleClick}>Add to playlist</button></li>) 
+            }<br />
             
             </ul>
-            {genreComponent}
+            {/* {genreComponent} */}
         </>
         
     )
