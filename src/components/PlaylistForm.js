5f059490d08ebb429fcccfe577e2309ae416edfa
bendesousa <bdsousa069@gmail.com>
2022-10-12 12:41:04 +0100

    added a playlist form (still lacks perfect functionailty

--- src/components/PlaylistForm.js
+++ src/components/PlaylistForm.js
@@ -1,13 +1,40 @@
+import { useState } from "react";
+
 const PlaylistForm = ({addPlaylist}) => {
 
-    const handleClick = () => {
-        addPlaylist("")
+    const [statePlaylist, setStatePlaylist] = useState(
+        {
+            name: "", 
+        }
+    )
+
+    const handleChange = (event) => {
+
+        let playlistName = event.target.name; 
+        let copiedPlaylist = {...statePlaylist}
+        copiedPlaylist[playlistName] = event.target.value;
+        setStatePlaylist(copiedPlaylist);
     }
 
-    return (
-        <>
-        
-        </>
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+        addPlaylist(statePlaylist)
+    }
+
+    return(
+            <form onSubmit={handleFormSubmit}>
+                <h3>Add a new playlist</h3>
+                
+                <input 
+                    type="text" 
+                    placeholder="playlist name" 
+                    name="name"
+                    onChange={handleChange}
+                    value={statePlaylist.name} />
+            
+                <button type="submit">OK</button>
+            </form>
+    
     )
 
 }
